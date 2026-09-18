216901eb803e20bb869581c26924f62c452a1460
Jackie He <labulubius@gmail.com>
2026-09-18 11:34:20 +0000

    Revert "Refresh app icon in KDE Breeze style"
    This reverts commit f7c20a2df10bcad816eaa05b9ee52526e14f0810.

--- app/manifest.ts
+++ app/manifest.ts
@@ -23,12 +23,6 @@ export default function manifest(): MetadataRoute.Manifest {
         type: "image/png",
         purpose: "any",
       },
-      {
-        src: "/icons/icon-maskable-512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
-      },
     ],
   };
 }
